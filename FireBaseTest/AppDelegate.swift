c0183c7d4e8d6c64222c6705252b8abcfd344a7e
Eяw!и Gontier <ierwinx@me.com>
2022-07-29 22:20:36 -0500

    se agrega la opción de recibir notificaciones Firebase

--- FireBaseTest/AppDelegate.swift
+++ FireBaseTest/AppDelegate.swift
@@ -1,11 +1,30 @@
 import UIKit
 import FirebaseCore
+import FirebaseMessaging
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        // MARK: - Init Firebase
         FirebaseApp.configure()
+        
+        //MARK: - Notificaciones
+        UNUserNotificationCenter.current().delegate = self
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(
+            options: authOptions,
+            completionHandler: { _, _ in }
+        )
+        application.registerForRemoteNotifications()
+        
+        //este es opcional pero podemos suscribir a una persona a un grupo de notificaciones
+        Messaging.messaging().subscribe(toTopic: "topicoTutoriales")
+        
+        // con este sabemos el identificador unico de cada persona pero no es nesesario, solo si queremos enviar notificaciones a una persona target
+        Messaging.messaging().delegate = self
+        
         return true
     }
 
@@ -17,4 +36,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
     }
 
+}
+
+//MARK: - Notificaciones Protocolo
+extension AppDelegate: UNUserNotificationCenterDelegate {
+
+    //Permite mostrar las notificaciones aunque la app este en primer plano
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.banner, .badge, .sound])
+    }
+
+}
+
+//MARK: - Notificaciones Protocolo Usuario Target
+extension AppDelegate: MessagingDelegate {
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        debugPrint("TOKEN: \(fcmToken ?? "")")
+    }
 }
